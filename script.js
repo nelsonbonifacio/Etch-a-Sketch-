c7cbe35fc31c1e4cd69cab5af5613c3beaac9ea2
Nelson Bonifacio <nbonifacio04@gmail.com>
2022-10-14 08:14:47 -0500

    Made a for loop to add a mouseenter and mouseleave event listener to all of the divs but the container. mouseenter call the randomcolor() function and mouseleave reverts the background-color to white.

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 const container = document.querySelector('.container');
 
+
 for (i = 1; i <= 256; i++) {
     let div = document.createElement('div');
-    div.setAttribute('style', 'width:20px; height:20px; background-color:red')
+    div.classList.add('box');
+    div.setAttribute('style', 'border:1px solid black')
     container.append(div);
-} 
+}
+
+const boxes = document.querySelectorAll('.box');
+
+for (const box of boxes) {
+    box.addEventListener('mouseenter', (e) => {
+        console.log(e);
+        e.target.style.backgroundColor = randomColor();
+    })
+    box.addEventListener('mouseleave', (e) => {
+        e.target.style.backgroundColor = 'white';
+    })
+}
+
+function randomColor(e) {
+    let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    return randomColor;
+}
