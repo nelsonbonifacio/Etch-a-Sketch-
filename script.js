c239d02c59ca187ebb2c40f8618dea97ec8addef
Nelson Bonifacio <nbonifacio04@gmail.com>
2022-10-15 10:51:20 -0500

    Made a functin called changeGrid which asks for ht enumber of squares per side and makes a new grid

--- script.js
+++ script.js
@@ -1,29 +1,50 @@
 const container = document.querySelector('.container');
+let boxes;
 
 
-for (i = 1; i <= 256; i++) {
-    let div = document.createElement('div');
-    div.classList.add('box');
-    div.setAttribute('style', 'border:1px solid black')
-    container.append(div);
+const grid = function (num) {
+    for (i = 1; i <= (num ? (num * num) : 256); i++) {
+        let div = document.createElement('div');
+        div.classList.add('box');
+        div.setAttribute('style', 'border:1px solid black')
+        container.append(div);
+        boxes = document.querySelectorAll('.box');
+    }
+}
+grid();
+
+const removeChild = function () {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
 }
 
-const boxes = document.querySelectorAll('.box');
 
-for (const box of boxes) {
-    box.addEventListener('mouseenter', (e) => {
-        console.log(e);
-        e.target.style.backgroundColor = randomColor();
-    })
-    box.addEventListener('mouseleave', (e) => {
-        e.target.style.backgroundColor = 'white';
-    })
+function colorOnTouch() {
+    for (const box of boxes) {
+        box.addEventListener('mouseenter', (e) => {
+            console.log(e);
+            e.target.style.backgroundColor = randomColor();
+        })
+    }
 }
+colorOnTouch();
 
-function randomColor(e) {
+const randomColor = function (e) {
     let randomColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     return randomColor;
 }
 
 const button = document.querySelector('button');
+button.addEventListener('click', changeGrid)
 
+// button.addEventListener('click', function())
+
+function changeGrid() {
+    const gridNumber = parseInt(prompt("what number of squares per side do you want? max:100", 10))
+    removeChild();
+    grid(gridNumber);
+    container.style.gridTemplateColumns = `repeat(${gridNumber}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${gridNumber}, 1fr)`;
+    colorOnTouch();
+}
